b0a997b2d054abdde62df4ced295e84855df22ca
JonaSMT <f.aracil.jonathan@gmail.com>
2021-11-19 12:22:43 +0100

    Programa Facturacion

---
+++ Facturacion/Facturacion/BBDD/ConexionBBDD.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Facturacion.BBDD
+{
+    internal class ConexionBBDD
+    {
+        NpgsqlConnection conexion = null;
+        public void ConectarBBDD()
+        {
+            string cadenaConexion = "Host=localhost;Username=postgres;Password=root;Database=Facturacion";
+            NpgsqlConnection conexion = new NpgsqlConnection(cadenaConexion);
+            conexion.Open();
+        }
+        public static void CargarLocalidad()
+        {
+            List<string> listaLocalidades = File.ReadAllLines(@"C:\Users\zumot\Desktop\Facturacion\Facturacion\Recursos\TablaPoblaciones.txt", Encoding.UTF8).ToList();
+            foreach (string localidad in listaLocalidades)
+            {
+                MessageBox.Show(localidad.Replace(" ", ";"));
+            }
+ 
+        }
+        public void DesconectarBBDD()
+        {
+            conexion.Close();
+        }
+    }
+}
---
+++ Facturacion/Facturacion/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Facturacion.BBDD;
+using Facturacion.Presentacion;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Facturacion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AnyadirFactura? anyadirFactura = default;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConexionBBDD.CargarLocalidad();
+        }
+
+        private void menuItemAnyadirFactura_Click(object sender, RoutedEventArgs e)
+        {
+            if (anyadirFactura == null)
+                anyadirFactura = new AnyadirFactura();
+            anyadirFactura.Width = wrpCuerpoPrincipal.Width;
+            anyadirFactura.Height = wrpCuerpoPrincipal.Height;
+            wrpCuerpoPrincipal.Children.Add(anyadirFactura);
+        }
+    }
+}
